e77534c4098975b94dea08f73c561845dc0c96ad
Zherdev Igor <cardinal3300@.com>
2025-10-16 19:48:54 +0300

    подключено субд postgresql, созданы модели product и category, отображения мигрированы, создана админка

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Отображение категорий в админке"""
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Отображение товаров в админке"""
+    list_display = ("id", "name", "purchase_price", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
